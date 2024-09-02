0f1702c30df7944de2d23ecfdcf42689b552a855
ishagupta <isha.gupta@menloschool.org>
2024-09-02 10:39:33 -0700

    Final

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,11 +1,18 @@
 Name: YOUR NAME HERE
 
-| Date   |    Time    |                                                               Update |
-|:-------|:----------:|---------------------------------------------------------------------:|
-| Aug 23 | 11:30-12pm | I started on #1. I realized that you can shift the letters over by 9 |
-|        |            |               I opened up IntelliJ and downloaded distribution code. |
-| Aug 23 |  1:30-2pm  |    I started coding #1. Having trouble wrapping around the alphabet. |
-| Aug 23 | 2pm-2:30pm |               I finished coding #1 and solved the wrap-around issue. |
+| Date   |    Time     |                                                               Update |
+|:-------|:-----------:|---------------------------------------------------------------------:|
+| Aug 23 | 11:30-12pm  | I started on #1. I realized that you can shift the letters over by 9 |
+|        |             |               I opened up IntelliJ and downloaded distribution code. |
+| Aug 23 |  1:30-2pm   |    I started coding #1. Having trouble wrapping around the alphabet. |
+| Aug 23 | 2pm-2:30pm  |               I finished coding #1 and solved the wrap-around issue. |
+| Aug 28 |  8:30-9pm   |                                                       Started on #2. |
+| Aug 28 |  9-9:30pm   |  Finished #2 and struggled to understand  bitwize operations for #3. |
+| Sept 1 |  4:15-4:45  |             Started #4 but struggled to figure out what to shift by. |
+| Sept 1 |  4:45-5:15  |                                                          Finished #4 |
+| Sept 1 |  5:15-5:45  |                                   Added comments and filled out log. |
+| Sept 1 | 6:00 - 6:30 |                        Finished #3 using parse int, but w/o bitwise. |
+| Sept 2 |  9:30-10am  |                Finalized and cleaned up code/comments and sumbitted. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -1,3 +1,7 @@
+// Isha Gupta
+// Sept 3, 2024
+// import java.sql.SQLOutput;
+
 import java.sql.SQLOutput;
 
 /**
@@ -6,69 +10,75 @@ import java.sql.SQLOutput;
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
  *
- * Completed by: YOUR NAME HERE
+ * Completed by: Isha Gupta
  */
 public class Riddler {
 
+    // Shift message over by 9
     public String decryptOne(String encrypted) {
         String decrypted = "";
-        int letter = 0;
-        System.out.println("A: " + (int)'A');
-        System.out.println("Z: " + (int)'Z');
-        System.out.println("a: " + (int)'a');
-        System.out.println("z: " + (int)'z');
+        int letter;
         for (int i = 0; i < encrypted.length(); i++) {
             letter = encrypted.charAt(i);
+
+            // If the letter is lowercase shift and map to it's corresponding lowercase letter
             if(letter >= 'a' && letter <= 'z'){
-                letter += 9;
-                if(letter > 'z'){
-                    decrypted += (char)(letter - 'z' - 1 + 'a');
-                }
-                else {
-                    decrypted += (char) (letter);
-                }
+                // Use mod 26 to solve the wrap-around issue
+                decrypted += (char)((letter - 'a' + 9) % 26 + 'a');
             }
             else if((letter >= 'A' && letter <= 'Z')){
-                letter += 9;
-                if(letter > 'Z'){
-                    decrypted += (char)(letter - 'Z' + 'A' -1);
-                }
-                else {
-                    decrypted += (char) (letter);
-                }
+                decrypted += (char)((letter - 'A' + 9) % 26 + 'A');
             }
+            // If it's not a letter (ie. space, comma, etc) do not shift
             else{
                 decrypted += (char) letter;
             }
-
         }
-
         System.out.println(decrypted);
-
         return decrypted;
     }
 
+    // Convert from Strings to ascii values to chars to decode
     public String decryptTwo(String encrypted) {
         String decrypted = "";
 
-        // TODO: Complete the decryptTwo() function.
+        // Divides the message based on where the spaces are
+        String[] letters = encrypted.split(" ");
+        // Creates an array of ints to store the converted strings above
+        int[] ascii = new int[letters.length];
 
+        for(int i = 0; i < letters.length; i++) {
+            // parseInt converts from text into ascii values (radix = base, we want base 10 for decimals)
+            ascii[i] = Integer.parseInt(letters[i], 10);
+            decrypted += (char)ascii[i];
+        }
+
+        System.out.println(decrypted);
         return decrypted;
     }
 
+    // Decode:  Strings --> binary --> ascii --> characters
     public String decryptThree(String encrypted) {
         String decrypted = "";
-
-        // TODO: Complete the decryptThree() function.
-
+        int ascii = 0;
+        // Add 8 each iteration bc each character is represented by 8 binary numbers
+        for(int i = 0; i < encrypted.length() - 8 ; i += 8){
+            ascii = Integer.parseInt(encrypted.substring(i, i+8), 2);
+            decrypted += (char)ascii;
+        }
+        System.out.println(decrypted);
         return decrypted;
     }
 
+    // Shift from Dingbat to letters
     public String decryptFour(String encrypted) {
         String decrypted = "";
+        for (int i = 0; i < encrypted.length(); i++) {
+            // Shifts each dingbat character into decimal and adds to decrypted message
+            decrypted += (char) (encrypted.charAt(i) - 9919);
+        }
 
-        // TODO: Complete the decryptFour() function.
-
+        System.out.println(decrypted);
         return decrypted;
     }
 }
